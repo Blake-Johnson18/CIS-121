df9dccfa91eb7f690a2c3fcc1cb3b6340609698c
Blake-Johnson18 <jc8822wf@go.minnstate.edu>
2022-09-01 13:24:20 -0500

    lab2

--- Lab2-9-1-22.py
+++ Lab2-9-1-22.py
@@ -1,16 +1,57 @@
-from curses.ascii import controlnames
+# name = "Blake"
+# lastname = "Johnson"
+
+# print("Blake")
+# print(name)
 
 
-name = "Blake"
-lastname = "Johnson"
-
-name = "B"
-
-print("Blake")
-print(name)
-
-print(coolname)
+# name = "B"
 
 
-coolname = "Lol"
+# coolname = "Lol"
+# print(coolname)
 
+# age = 26
+
+# print(type(age))
+
+
+# distance = 34.5
+
+# print(type(distance))
+
+# print(age + distance)
+
+# # Lab 2 part 1
+
+# dog_years = 7
+
+# # Create a variable to stroe the humans age
+# human_age = int(input("How old are you: "))
+# # Calculate how old is the human in dog years
+# dog_age = dog_years*human_age
+# print("An age", human_age, "human should be", dog_age, "in dogs age.")
+
+
+# Create a script that asks the user for name,lastname and age. Print out the info as follows
+# Hi Kendrick Morales, You're 23 years old, damn.
+
+# name = input("What's your first name? ")
+# lastname = input("What's your last name? ")
+# age = int(input("How old are you? "))
+
+# print("Hi", name, lastname +",", "You're", age, "years old, Damn.")
+
+# human_age = float(input("How old are you? "))
+# Horse_age = 3 * ((((human_age**2)-47)/7)+12)
+
+# print("An age", human_age, "human should be about", Horse_age, "in horse age.")
+
+# To calculate a humans height you do the following: age^2
+
+# Print out the humans age and estimated height
+
+# human_age = int(input("How old are you: ")) # taking the age of the person
+# height = human_age**2 # Calculates and stores height estimate
+
+# print("Someone that is", human_age, "years old, should be around", height, "tall.")
