d25481da8429b6e3d9ebed4df8d730a9221ef6fa
Blake-Johnson18 <jc8822wf@go.minnstate.edu>
2022-09-04 15:32:43 -0500

    additions

--- Lab2-9-1-22.py
+++ Lab2-9-1-22.py
@@ -1,40 +1,49 @@
-# name = "Blake"
-# lastname = "Johnson"
+from math import floor
+# # name = "Blake"
+# # lastname = "Johnson"
 
-# print("Blake")
-# print(name)
-
-
-# name = "B"
+# # print("Blake")
+# # print(name)
 
 
-# coolname = "Lol"
-# print(coolname)
-
-# age = 26
-
-# print(type(age))
+# # name = "B"
 
 
-# distance = 34.5
+# # coolname = "Lol"
+# # print(coolname)
 
-# print(type(distance))
+# # age = 26
 
-# print(age + distance)
+# # print(type(age))
+
+
+# # distance = 34.5
+
+# # print(type(distance))
+
+# # print(age + distance)
 
 # # Lab 2 part 1
 
-# dog_years = 7
+dog_years = 7
 
 # # Create a variable to stroe the humans age
-# human_age = int(input("How old are you: "))
+human_age = float(input("How old are you: "))
 # # Calculate how old is the human in dog years
-# dog_age = dog_years*human_age
-# print("An age", human_age, "human should be", dog_age, "in dogs age.")
+dog_age_years = dog_years*human_age
+dog_age_years_r = dog_years%human_age
+dog_age_months = dog_age_years_r*12
+dog_age_months_r = dog_age_months%1
+dog_age_days = dog_age_months_r*31
+# print(floor(dog_age_years))
+# print(floor(dog_age_months))
+# print(dog_age_months_r)
+print(round(dog_age_days))
+print("An age", human_age, "human should be", floor(dog_age_years), "years, and", floor(dog_age_months), "months, and", round(dog_age_days), "dyas in dog age.")
 
 
-# Create a script that asks the user for name,lastname and age. Print out the info as follows
-# Hi Kendrick Morales, You're 23 years old, damn.
+# # Create a script that asks the user for name,lastname and age. Print out the info as follows
+# # Hi Kendrick Morales, You're 23 years old, damn.
 
 # name = input("What's your first name? ")
 # lastname = input("What's your last name? ")
@@ -42,16 +51,16 @@
 
 # print("Hi", name, lastname +",", "You're", age, "years old, Damn.")
 
-# human_age = float(input("How old are you? "))
+# # human_age = float(input("How old are you? "))
 # Horse_age = 3 * ((((human_age**2)-47)/7)+12)
 
 # print("An age", human_age, "human should be about", Horse_age, "in horse age.")
 
-# To calculate a humans height you do the following: age^2
+# # To calculate a humans height you do the following: age^2
 
-# Print out the humans age and estimated height
+# # Print out the humans age and estimated height
 
-# human_age = int(input("How old are you: ")) # taking the age of the person
+# # human_age = int(input("How old are you: ")) # taking the age of the person
 # height = human_age**2 # Calculates and stores height estimate
 
 # print("Someone that is", human_age, "years old, should be around", height, "tall.")
