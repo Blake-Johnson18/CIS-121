4e3c3bdeee1d276adc47cd7a70bdd6269a4789f3
Blake-Johnson18 <jc8822wf@go.minnstate.edu>
2022-10-27 13:24:11 -0500

    lab 10

---
+++ DiceGameProgram_StudentTemplate (1).py
@@ -0,0 +1,18 @@
+"""
+Blake Johnson
+10/27/22
+
+This program...
+"""
+from DiceGameClasses import HighTwoGame
+
+
+print("Game one:")
+game1 = HighTwoGame("Matt", "Ashley")
+game1.playOneGame()
+
+print("")
+print("Game two:")
+game2 = HighTwoGame("Dexter", "Eugene")
+game2.playOneGame()
+
